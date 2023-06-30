4937fd505bac55f67058f83318ed34f79cd896b6
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2023-06-30 09:46:32 -0400

    Learning MEAN stack and creating a blog Website

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from "@angular/core";
+
+import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post content'},
+  //   {title: 'Second Post', content: 'This is the second post content'},
+  //   {title: 'Third Post', content: 'This is the third post content'}
+  // ]
+  @Input() posts: Post[] = [];
+
+  constructor(public postsService: PostsService) {}
+}
