734637cbb6275b2a34aa73e6eecc0041e7147c5a
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2023-08-04 21:55:36 -0400

    Reorganizing backend routes.

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
@@ -30,61 +31,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    res.status(200).json({message: "Update Successful!"});
-  });
-});
-
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    console.log(createdPost);
-    res.status(201).json({
-      message: 'Post Added successfully',
-      postId: createdPost._id
-    });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'Posts successfully fetched!',
-        posts: documents
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({message: 'Post not found!'});
-      }
-  });
-})
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  console.log(req.params.id);
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  }).catch(error =>  {
-    console.log(error); // Check if there's any error in the server logs
-    res.status(500).json({ error: "An error occurred while deleting the post." });
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
