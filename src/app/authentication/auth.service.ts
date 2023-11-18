d92d76f824ad39b471fd92e3cef8abef25d7ef74
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2023-11-18 17:20:45 -0500

    Sending Token to Frontend.

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { AuthData } from "./auth-data.model";
+import { response } from "express";
 
 @Injectable({providedIn: "root"})
 export class AuthService{
@@ -12,5 +13,13 @@ export class AuthService{
       .subscribe(response =>{
         console.log(response);
       });
+  }
+
+  login(email: string, password: string){
+    const authData: AuthData = {email: email, password: password}
+    this.http.post("http://localhost:3000/api/user/login", authData)
+      .subscribe(response => {
+        console.log(response);
+      })
   }
 }
