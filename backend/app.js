993186f922fc05be0061f14c176c326701bb4ba5
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2023-11-18 12:27:46 -0500

    Connecting Angular to Backend.

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://Shaishav23:shaishav23@cluster0.b2lkjqo.mongodb.net/node-angular?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://Shaishav23:shaishav23@cluster0.b2lkjqo.mongodb.net/?retryWrites=true&w=majority")
   .then(() => {
     console.log('Connected to database');
   })
@@ -36,6 +36,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
-app.use("api/user", userRoutes);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,28 @@
 const express = require("express");
-const { route } = require("./posts");
+const User = require('../models/user');
 
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 router.post("/signup", (req, res, next) => {
-
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user =new User({
+      email: req.body.email,
+      password: hash
+      });
+      user.save()
+        .then(result => {
+          res.status(201).json({
+            message: "User Created",
+            result: result
+          });
+        });
+    }).catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 module.exports = router;
