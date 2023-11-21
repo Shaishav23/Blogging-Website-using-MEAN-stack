98cc66deee9d8328730366af5fb354fbaecf66e1
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2023-11-20 20:20:38 -0500

    Improving UI to reflect user authentication.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+
+import { AuthService } from "../authentication/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+  private authLiatenerSubs: Subscription;
+  constructor( private authService: AuthService){}
+
+  ngOnInit() {
+    this.authLiatenerSubs = this.authService
+    .getAuthStatusListener()
+    .subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
+    }
+  ngOnDestroy() {
+    this.authLiatenerSubs.unsubscribe();
+  }
+}
