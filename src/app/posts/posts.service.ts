6a8e2c5d2589f26942d87e1fe572821b8fec0e7a
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2023-12-24 15:53:05 -0500

    Using UserId on the frontend.

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -49,7 +49,13 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{_id: string, title: string, content: string, imagePath: string}>("http://localhost:3000/api/posts/" + id);
+    return this.http.get<{
+      _id: string;
+      title: string;
+      content: string;
+      imagePath: string;
+      creator: string;
+    }>("http://localhost:3000/api/posts/" + id);
   }
 
   //Add's dependency injection
@@ -82,7 +88,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http
