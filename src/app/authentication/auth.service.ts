8d9de8bba47cc90ab38d8e2c505ac8acc2b99a33
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2023-12-24 22:43:40 -0500

    testing different places to handle errors.

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -33,15 +33,20 @@ export class AuthService{
 
   createUser(email: string, password: string){
     const authData: AuthData = {email: email, password: password}
-    this.http.post("http://localhost:3000/api/user/signup", authData)
-      .subscribe(response =>{
-        console.log(response);
-      });
-  }
+    return this.http
+    .post("http://localhost:3000/api/user/signup", authData)
+    .subscribe({
+      next: ()=> {this.router.navigate(['/'])},
+      error: (err)=> {this.authStatusListener.next(false);}
+    });
+    }
 
   login(email: string, password: string){
     const authData: AuthData = {email: email, password: password}
-    this.http.post<{token: string, expiresIn: number, userId: string }>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string, expiresIn: number, userId: string }>(
+      "http://localhost:3000/api/user/login",
+      authData
+      )
       .subscribe(response => {
         const token = response.token;
         this.token = token;
@@ -56,6 +61,8 @@ export class AuthService{
           this.saveAuthData(token, expirationDate, this.userId);
           this.router.navigate(['/']);
         }
+      }, error => {
+        this.authStatusListener.next(false);
       });
 
   }
